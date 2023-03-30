52260417ac7f8541c08e63d64aa866ad39068c5d
kenko <kyoshitomi.cursor@gmail.com>
2023-03-30 22:39:57 +0900

    Fix bookshelf page

--- portfolio/src/components/Bookshelf/Bookshelf.tsx
+++ portfolio/src/components/Bookshelf/Bookshelf.tsx
@@ -2,9 +2,9 @@ import Books from "./books.json";
 
 const Bookshelf = () => {
   return (
-    <section className="bg-secondary text-white px-5 py-32" id="about">
+    <section className="bg-secondary text-white px-5 py-32" id="bookshelf">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
-        <div className="about-info mb-5">
+        <div className="bookshelf-info mb-5">
           <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[170px] pb-2 border-[#FF9036]">
             Bookshelf
           </h2>
