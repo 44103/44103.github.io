30a1f0311cbb7e0c2b935e7a31445c495634b4e8
kenko <kyoshitomi.cursor@gmail.com>
2023-04-01 19:29:16 +0900

    Modify about page

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -7,11 +7,19 @@ const About = () => {
             About Me
           </h2>
 
-          <p className="pb-5"></p>
+          <p className="pb-5">
+            福岡県出身、東京都在住。亥年・牡牛座なだけあって、どんな技術にも猪突猛進に取り組みます。
+          </p>
 
-          <p className="pb-5"></p>
+          <p className="pb-5">
+            ガジェット集めが趣味の一つで、最近は自作キーボードを作りました。
+            <br />
+            Arch Linux + i3wm + Vimium + VSCodeで9割の操作はキーボードのみです。
+          </p>
 
-          <p className="pb-5"></p>
+          <p className="pb-5">
+            環境構築に苦しむのは嫌なので、軽量で他のマシンでも再現性のあるDockerに興味をもち、個人ではすべての開発環境をDockerで用意しています。
+          </p>
         </div>
       </div>
     </section>
--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -1,24 +1,21 @@
-import VtrImg from "../assets/vtr.png";
+import VtrImg from "../assets/vtr-alpha.png";
 
 const Home = () => {
   return (
     <section className="bg-primary text-white px-5 py-32">
       <div className="container mx-auto grid md:grid-cols-2 items-center jusify-center md:justify-between">
         <div className="home-info pb-5 md:pb-0">
-          <h1 className="text-4xl lg:text-6xl">
+          <h1 className="text-4xl lg:text-6xl mb-10">
             Hi, <br />
             I'm <span className="text-accent">K</span>enko <br />
             Backend Developer
           </h1>
 
-          <p className="py-5">
-            福岡県出身、東京都在住。どんな技術にも猪突猛進に取り組みます（亥年・牡牛座）。
-          </p>
           <a
             href="/#about"
             className="btn bg-accent border-2 border-[#FF9036] text-white px-6 py-3 hover:bg-transparent"
           >
-            See Projects
+            See About Me
           </a>
         </div>
 
@@ -26,7 +23,7 @@ const Home = () => {
           <img
             src={VtrImg}
             alt="icon illustration"
-            className="rounded-full w-[80%] ml-auto"
+            className="w-[80%] ml-auto"
           />
         </div>
       </div>
